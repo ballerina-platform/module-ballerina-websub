e0fc5175e7764fa70a953d904ea19002f727e872
ayeshLK <ayesh9303@gmail.com>
2021-02-01 21:52:49 +0530

    Fix styling issues in the native module

--- websub-native/src/main/java/io/ballerina/stdlib/websub/SubscriberNativeOperationHandler.java
+++ websub-native/src/main/java/io/ballerina/stdlib/websub/SubscriberNativeOperationHandler.java
@@ -15,7 +15,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package io.ballerina.stdlib.websub;
 
 import io.ballerina.runtime.api.Environment;
@@ -24,8 +23,8 @@ import io.ballerina.runtime.api.async.StrandMetadata;
 import io.ballerina.runtime.api.creators.ValueCreator;
 import io.ballerina.runtime.api.types.MethodType;
 import io.ballerina.runtime.api.utils.StringUtils;
-import io.ballerina.runtime.api.values.BError;
 import io.ballerina.runtime.api.values.BArray;
+import io.ballerina.runtime.api.values.BError;
 import io.ballerina.runtime.api.values.BMap;
 import io.ballerina.runtime.api.values.BObject;
 import io.ballerina.runtime.api.values.BString;
@@ -33,7 +32,11 @@ import io.ballerina.runtime.api.values.BString;
 import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
 
+/**
+ * {@code SubscriberNativeOperationHandler} handles the native method execution.
+ */
 public class SubscriberNativeOperationHandler {
+
     public static BArray getServiceMethodNames(BObject bSubscriberService) {
         ArrayList<BString> methodNamesList = new ArrayList<>();
         for (MethodType method : bSubscriberService.getType().getMethods()) {
@@ -42,16 +45,21 @@ public class SubscriberNativeOperationHandler {
         return ValueCreator.createArrayValue(methodNamesList.toArray(BString[]::new));
     }
 
-    public static Object callOnSubscriptionVerificationMethod(Environment env, BObject bSubscriberService, BMap<BString, Object> message) {
-        return invokeRemoteFunction(env, bSubscriberService, message, "callOnSubscriptionVerificationMethod", "onSubscriptionVerification");
+    public static Object callOnSubscriptionVerificationMethod(Environment env, BObject bSubscriberService, 
+                                                              BMap<BString, Object> message) {
+        return invokeRemoteFunction(env, bSubscriberService, message, 
+                                    "callOnSubscriptionVerificationMethod", "onSubscriptionVerification");
     }   
 
     public static Object callOnSubscriptionDeniedMethod(Environment env, BObject bSubscriberService, BError message) {
-        return invokeRemoteFunction(env, bSubscriberService, message, "callOnSubscriptionDeniedMethod", "onSubscriptionValidationDenied"); 
+        return invokeRemoteFunction(env, bSubscriberService, message, 
+                                    "callOnSubscriptionDeniedMethod", "onSubscriptionValidationDenied"); 
     }
 
-    public static Object callOnEventNotificationMethod(Environment env, BObject bSubscriberService, BMap<BString, Object> message) {
-        return invokeRemoteFunction(env, bSubscriberService, message, "callOnEventNotificationMethod", "onEventNotification"); 
+    public static Object callOnEventNotificationMethod(Environment env, BObject bSubscriberService, 
+                                                       BMap<BString, Object> message) {
+        return invokeRemoteFunction(env, bSubscriberService, message, 
+                                    "callOnEventNotificationMethod", "onEventNotification"); 
     }
 
     private static Object invokeRemoteFunction(Environment env, BObject bSubscriberService, Object message,
