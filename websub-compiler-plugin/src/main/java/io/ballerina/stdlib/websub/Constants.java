3ee29dc9f45b0e32e6c053ad55294e06e26e99d1
ayeshLK <ayesh9303@gmail.com>
2021-04-08 12:18:13 +0530

    Update logic for websub-service identification

--- websub-compiler-plugin/src/main/java/io/ballerina/stdlib/websub/Constants.java
+++ websub-compiler-plugin/src/main/java/io/ballerina/stdlib/websub/Constants.java
@@ -4,19 +4,21 @@ package io.ballerina.stdlib.websub;
  * {@code Constants} contains the public constants to be used.
  */
 public interface Constants {
-    public static final String MODULE_NAME = "websub";
-    public static final String SERVICE_ANNOTATION_NAME = "SubscriberServiceConfig";
+    String ORG_NAME = "ballerina";
+    String MODULE_NAME = "websub";
+    String SERVICE_ANNOTATION_NAME = "SubscriberServiceConfig";
 
-    public static final String ON_SUBSCRIPTION_VALIDATION_DENIED = "onSubscriptionValidationDenied";
-    public static final String ON_SUBSCRIPTION_VERIFICATION = "onSubscriptionVerification";
-    public static final String ON_EVENT_NOTIFICATION = "onEventNotification";
+    String ON_SUBSCRIPTION_VALIDATION_DENIED = "onSubscriptionValidationDenied";
+    String ON_SUBSCRIPTION_VERIFICATION = "onSubscriptionVerification";
+    String ON_EVENT_NOTIFICATION = "onEventNotification";
 
-    public static final String SUBSCRIPTION_DENIED_ERROR = "websub:SubscriptionDeniedError";
-    public static final String SUBSCRIPTION_VERIFICATION = "websub:SubscriptionVerification";
-    public static final String CONTENT_DISTRIBUTION_MESSAGE = "websub:ContentDistributionMessage";
-    public static final String ACKNOWLEDGEMENT = "websub:Acknowledgement";
-    public static final String SUBSCRIPTION_VERIFICATION_SUCCESS = "websub:SubscriptionVerificationSuccess";
-    public static final String SUBSCRIPTION_VERIFICATION_ERROR = "websub:SubscriptionVerificationError";
-    public static final String SUBSCRIPTION_DELETED_ERROR = "websub:SubscriptionDeletedError";
-    public static final String OPTIONAL = "?";
+    String SUBSCRIPTION_DENIED_ERROR = "websub:SubscriptionDeniedError";
+    String SUBSCRIPTION_VERIFICATION = "websub:SubscriptionVerification";
+    String CONTENT_DISTRIBUTION_MESSAGE = "websub:ContentDistributionMessage";
+    String ACKNOWLEDGEMENT = "websub:Acknowledgement";
+    String SUBSCRIPTION_VERIFICATION_SUCCESS = "websub:SubscriptionVerificationSuccess";
+    String SUBSCRIPTION_VERIFICATION_ERROR = "websub:SubscriptionVerificationError";
+    String SUBSCRIPTION_DELETED_ERROR = "websub:SubscriptionDeletedError";
+
+    String OPTIONAL = "?";
 }
--- websub-compiler-plugin/src/main/java/io/ballerina/stdlib/websub/validator/ServiceDeclarationValidator.java
+++ websub-compiler-plugin/src/main/java/io/ballerina/stdlib/websub/validator/ServiceDeclarationValidator.java
@@ -1,12 +1,13 @@
 package io.ballerina.stdlib.websub.validator;
 
 import io.ballerina.compiler.api.symbols.AnnotationSymbol;
+import io.ballerina.compiler.api.symbols.ModuleSymbol;
 import io.ballerina.compiler.api.symbols.ServiceDeclarationSymbol;
-import io.ballerina.compiler.api.symbols.SymbolKind;
+import io.ballerina.compiler.api.symbols.Symbol;
+import io.ballerina.compiler.api.symbols.TypeDescKind;
 import io.ballerina.compiler.api.symbols.TypeReferenceTypeSymbol;
 import io.ballerina.compiler.api.symbols.TypeSymbol;
-import io.ballerina.compiler.api.symbols.VariableSymbol;
-import io.ballerina.compiler.syntax.tree.ExpressionNode;
+import io.ballerina.compiler.api.symbols.UnionTypeSymbol;
 import io.ballerina.compiler.syntax.tree.FunctionDefinitionNode;
 import io.ballerina.compiler.syntax.tree.Node;
 import io.ballerina.compiler.syntax.tree.NodeList;
@@ -18,12 +19,10 @@ import io.ballerina.compiler.syntax.tree.SeparatedNodeList;
 import io.ballerina.compiler.syntax.tree.ServiceDeclarationNode;
 import io.ballerina.compiler.syntax.tree.SyntaxKind;
 import io.ballerina.compiler.syntax.tree.Token;
-import io.ballerina.projects.Document;
 import io.ballerina.projects.plugins.AnalysisTask;
 import io.ballerina.projects.plugins.SyntaxNodeAnalysisContext;
 import io.ballerina.stdlib.websub.Constants;
 import io.ballerina.stdlib.websub.WebSubDiagnosticCodes;
-import io.ballerina.tools.text.LinePosition;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -74,38 +73,35 @@ public class ServiceDeclarationValidator implements AnalysisTask<SyntaxNodeAnaly
     @Override
     public void perform(SyntaxNodeAnalysisContext context) {
         ServiceDeclarationNode serviceNode = (ServiceDeclarationNode) context.node();
-        if (isWebSubService(context, serviceNode)) {
-            validateServiceAnnotation(context, serviceNode);
-            List<FunctionDefinitionNode> availableFunctionDeclarations = serviceNode.members().stream()
-                    .filter(member -> member.kind() == SyntaxKind.OBJECT_METHOD_DEFINITION)
-                    .map(member -> (FunctionDefinitionNode) member).collect(Collectors.toList());
-            validateRequiredMethodsImplemented(context, availableFunctionDeclarations, serviceNode.location());
-            availableFunctionDeclarations.forEach(fd -> {
-                validateRemoteQualifier(context, fd);
-                validateAdditionalMethodImplemented(context, fd);
-                validateMethodParameters(context, fd);
-                validateMethodReturnTypes(context, fd);
-            });
+        Optional<Symbol> serviceDeclarationOpt = context.semanticModel().symbol(serviceNode);
+        if (serviceDeclarationOpt.isPresent()) {
+            ServiceDeclarationSymbol serviceDeclarationSymbol = (ServiceDeclarationSymbol) serviceDeclarationOpt.get();
+            if (isWebSubService(serviceDeclarationSymbol)) {
+                validateServiceAnnotation(context, serviceNode, serviceDeclarationSymbol);
+                List<FunctionDefinitionNode> availableFunctionDeclarations = serviceNode.members().stream()
+                        .filter(member -> member.kind() == SyntaxKind.OBJECT_METHOD_DEFINITION)
+                        .map(member -> (FunctionDefinitionNode) member).collect(Collectors.toList());
+                validateRequiredMethodsImplemented(context, availableFunctionDeclarations, serviceNode.location());
+                availableFunctionDeclarations.forEach(fd -> {
+                    validateRemoteQualifier(context, fd);
+                    validateAdditionalMethodImplemented(context, fd);
+                    validateMethodParameters(context, fd);
+                    validateMethodReturnTypes(context, fd);
+                });
+            }
         }
     }
 
-    private void validateServiceAnnotation(SyntaxNodeAnalysisContext context, ServiceDeclarationNode serviceNode) {
-        Optional<ServiceDeclarationSymbol> serviceDeclarationOptional = context.semanticModel().moduleSymbols()
+    private void validateServiceAnnotation(SyntaxNodeAnalysisContext context, ServiceDeclarationNode serviceNode,
+                                           ServiceDeclarationSymbol serviceDeclarationSymbol) {
+        Optional<AnnotationSymbol> subscriberServiceAnnotationOptional = serviceDeclarationSymbol.annotations()
                 .stream()
-                .filter(e -> e.kind() == SymbolKind.SERVICE_DECLARATION)
-                .map(s -> (ServiceDeclarationSymbol) s)
-                .findFirst();
-        if (serviceDeclarationOptional.isPresent()) {
-            ServiceDeclarationSymbol serviceDeclarationSymbol = serviceDeclarationOptional.get();
-            Optional<AnnotationSymbol> subscriberServiceAnnotationOptional = serviceDeclarationSymbol.annotations()
-                    .stream()
-                    .filter(annotationSymbol ->
-                            annotationSymbol.getName().orElse("").equals(Constants.SERVICE_ANNOTATION_NAME)
-                    ).findFirst();
-            if (subscriberServiceAnnotationOptional.isEmpty()) {
-                WebSubDiagnosticCodes errorCode = WebSubDiagnosticCodes.WEBSUB_101;
-                updateContext(context, errorCode, serviceNode.location());
-            }
+                .filter(annotationSymbol ->
+                        annotationSymbol.getName().orElse("").equals(Constants.SERVICE_ANNOTATION_NAME)
+                ).findFirst();
+        if (subscriberServiceAnnotationOptional.isEmpty()) {
+            WebSubDiagnosticCodes errorCode = WebSubDiagnosticCodes.WEBSUB_101;
+            updateContext(context, errorCode, serviceNode.location());
         }
     }
 
@@ -212,34 +208,32 @@ public class ServiceDeclarationValidator implements AnalysisTask<SyntaxNodeAnaly
         }
     }
 
-    private boolean isWebSubService(SyntaxNodeAnalysisContext context, ServiceDeclarationNode serviceNode) {
-        SeparatedNodeList<ExpressionNode> expressions = serviceNode.expressions();
-        Optional<TypeSymbol> moduleTypeDescriptor = expressions.stream()
-                .filter(this::isListenerAttachingExpression)
-                .map(e -> getTypeSymbol(context, e))
-                .findFirst();
-        if (moduleTypeDescriptor.isEmpty()) {
-            return false;
-        } else {
-            TypeSymbol moduleType = moduleTypeDescriptor.get();
-            String signature = moduleType.signature();
-            return signature.contains(Constants.MODULE_NAME);
-        }
+    private boolean isWebSubService(ServiceDeclarationSymbol serviceDeclarationSymbol) {
+        return serviceDeclarationSymbol.listenerTypes().stream().anyMatch(this::isWebSubListener);
     }
 
-    private boolean isListenerAttachingExpression(ExpressionNode e) {
-        return e.kind() == SyntaxKind.EXPLICIT_NEW_EXPRESSION || e.kind() == SyntaxKind.SIMPLE_NAME_REFERENCE;
+    private boolean isWebSubListener(TypeSymbol listenerType) {
+        if (listenerType.typeKind() == TypeDescKind.UNION) {
+            return ((UnionTypeSymbol) listenerType).memberTypeDescriptors().stream()
+                    .filter(typeDescriptor -> typeDescriptor instanceof TypeReferenceTypeSymbol)
+                    .map(typeReferenceTypeSymbol -> (TypeReferenceTypeSymbol) typeReferenceTypeSymbol)
+                    .anyMatch(typeReferenceTypeSymbol ->
+                            typeReferenceTypeSymbol.getModule().isPresent()
+                                    && isWebSub(typeReferenceTypeSymbol.getModule().get()
+                    ));
+        }
+
+        if (listenerType.typeKind() == TypeDescKind.TYPE_REFERENCE) {
+            Optional<ModuleSymbol> moduleOpt = ((TypeReferenceTypeSymbol) listenerType).typeDescriptor().getModule();
+            return moduleOpt.isPresent() && isWebSub(moduleOpt.get());
+        }
+
+        return false;
     }
 
-    private TypeSymbol getTypeSymbol(SyntaxNodeAnalysisContext context, ExpressionNode e) {
-        Document currentDocument = context.currentPackage().getDefaultModule().document(context.documentId());
-        LinePosition lineStart = e.lineRange().startLine();
-        if (e.kind() == SyntaxKind.EXPLICIT_NEW_EXPRESSION) {
-            return ((TypeReferenceTypeSymbol) context.semanticModel().symbol(currentDocument, lineStart).get())
-                    .typeDescriptor();
-        } else {
-            return ((VariableSymbol) context.semanticModel().symbol(currentDocument, lineStart).get())
-                    .typeDescriptor();
-        }
+    private boolean isWebSub(ModuleSymbol moduleSymbol) {
+        Optional<String> moduleNameOpt = moduleSymbol.getName();
+        return moduleNameOpt.isPresent() && Constants.MODULE_NAME.equals(moduleNameOpt.get())
+                && Constants.ORG_NAME.equals(moduleSymbol.id().orgName());
     }
 }
