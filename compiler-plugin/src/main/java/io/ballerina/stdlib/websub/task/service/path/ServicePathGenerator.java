0b8acfc51547940274ac74b3341d55e47b01fb2c
ayeshLK <ayesh9303@gmail.com>
2021-11-16 09:58:39 +0530

    Add doc-comments to ServicePathGenerator interface methods

--- compiler-plugin/src/main/java/io/ballerina/stdlib/websub/task/service/path/ServicePathGenerator.java
+++ compiler-plugin/src/main/java/io/ballerina/stdlib/websub/task/service/path/ServicePathGenerator.java
@@ -27,7 +27,22 @@ import java.nio.file.Path;
  * {@code ServicePathGenerator} generates unique service path for subscriber service and saves into a resource file.
  */
 public interface ServicePathGenerator {
+    /**
+     * Generates the service-path for the provided `websub:SubscriberService`.
+     *
+     * @param packageId - PackageId of the current ballerina project
+     * @param currentProjectRoot - Project source root path
+     * @param serviceId - ServiceId of the curreng `websub:SubscriberService`
+     * @throws ServicePathGeneratorException - If there is any runtime exception while generating the service-path
+     */
     void generate(PackageId packageId, Path currentProjectRoot, int serviceId) throws ServicePathGeneratorException;
 
+    /**
+     * Checks whether current {@code ServicePathGenerator} instance supports the provided ballerina project-type.
+     *
+     * @param projectType - Ballerina project-type of the current project
+     * @return - {@code true} if current {@code ServicePathGenerator} is compatible with the provided project-type
+     *           or else {@code false}
+     */
     boolean isSupported(ProjectKind projectType);
 }
